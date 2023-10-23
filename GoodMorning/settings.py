0518649867b3847f71ecd0148b36cb296c4584ae
Sumit Yadav <syadav1@greencellmobility.com>
2023-10-23 16:44:19 +0530

    added ALLOWED_HOST

--- GoodMorning/settings.py
+++ GoodMorning/settings.py
@@ -5,10 +5,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-m(gnw2qx#3l)jo!_yup22iuuu$))q4$s0$jnhs&=b*_bq!0gv0'
 
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['https://welcometogcm.azurewebsites.net/']
+ALLOWED_HOSTS = ['welcometogcm.azurewebsites.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
