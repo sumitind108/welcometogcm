e8d498b259b2c3f11724a8687c37b12fec6e8b21
Sumit Yadav <syadav1@greencellmobility.com>
2023-10-23 17:19:12 +0530

    DEBUG

--- GoodMorning/settings.py
+++ GoodMorning/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-m(gnw2qx#3l)jo!_yup22iuuu$))q4$s0$jnhs&=b*_bq!0gv0'
 
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['welcometogcm.azurewebsites.net']
